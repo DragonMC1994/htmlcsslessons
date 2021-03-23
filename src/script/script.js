7f771115584dd97671292afda536b29b61f686ec
Andrey <dragon3d3@yandex.ru>
2021-03-23 22:11:06 +0300

    to PC. slider done

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true
-      });
-  });
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/4sec/left.svg"></button>',
+        nextArrow: '<button type=button" class="slick-next"><img src="img/4sec/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
