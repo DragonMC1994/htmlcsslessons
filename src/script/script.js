714df014fe90cf863c78bf0cffb7551a14da197e
Andrey <dragon3d3@yandex.ru>
2021-04-04 13:18:04 +0300

    add scroll

--- src/script/script.js
+++ src/script/script.js
@@ -118,4 +118,19 @@ $(document).ready(function () {
             return false;
         });
     });
+    // scroll and page up
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1000) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
 });
